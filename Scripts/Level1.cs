b303c2bf05e0f09bd1e76a8da9337e884ab32544
Pacofication <paco@pacofication.com>
2020-06-04 22:28:47 -0400

    Added my enemy to Level1

--- Scripts/Level1.cs
+++ Scripts/Level1.cs
@@ -1,28 +1,62 @@
 using Godot;
-using System;
 
 public class Level1 : Node2D
 {
+    [Export] public PackedScene EnemyPaco;
+
+    [Export] public int EnemyPacoPackCount = 5;
+
+    private Node2D _gameNode;
+    
     private Menu _menu;
 
-    private bool isCancelPressed = false;
+    private bool _isCancelPressed;
+
+    private Timer _enemyPacoSpawnTimer;
+
+    private int _enemyPacoSpawnCount;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        _gameNode = GetNode<Node2D>("Game");
         _menu = GetNode<Menu>("Menu");
+        
+        _isCancelPressed = false;
+        _enemyPacoSpawnCount = 0;
+        _enemyPacoSpawnTimer = GetNode<Timer>("Game/EnemyPacoSpawnTimer");
     }
 
     public override void _Process(float delta)
     {
-        if (Input.IsActionPressed("ui_cancel") && !isCancelPressed)
+        if (Input.IsActionPressed("ui_cancel") && !_isCancelPressed)
         {
-            isCancelPressed = true;
+            _isCancelPressed = true;
             MenuToggle();
         }
         else if (!Input.IsActionPressed("ui_cancel"))
         {
-            isCancelPressed = false;
+            _isCancelPressed = false;
+        }
+    }
+
+    public void OnEnemyPacoTimerTimeout()
+    {
+        _enemyPacoSpawnTimer.Start();
+    }
+
+    public void OnEnemyPacoSpawnTimerTimeout()
+    {
+        if (_enemyPacoSpawnCount < EnemyPacoPackCount)
+        {
+            _enemyPacoSpawnCount++;
+            var enemyPacoInstance = (EnemyPacoPath) EnemyPaco.Instance();
+            _gameNode.AddChild(enemyPacoInstance);
+        }
+        else
+        {
+            _enemyPacoSpawnCount = 0;
+            _enemyPacoSpawnTimer.Stop();
         }
     }
 

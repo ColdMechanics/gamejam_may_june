d1b795ba04c72ada323728c7894303544a069293
Alexandre Beaudoin <abeaudoin@coldmechanics.com>
2020-06-11 22:23:55 -0400

    Make the boss target the player.

--- Scripts/Ennemies/Boss.cs
+++ Scripts/Ennemies/Boss.cs
@@ -1,10 +1,21 @@
 using Godot;
-using System;
 
 public class Boss : EnnemyBase
 {
     public override void _Ready()
     {
         // TODO target the player
+    }
+
+    public override void _Process(float delta)
+    {
+        if (Level1.Player is null) return;
+
+        Vector2 velocity = this.GlobalPosition.DirectionTo(Level1.Player.GlobalPosition);
+        
+        if((this.GlobalPosition - Level1.Player.GlobalPosition).Length() < 100) return;
+        
+        this.Position += (velocity * this.Speed * delta);
+        LookAt(Level1.Player.GlobalPosition);
     }
 }
